15b82593c7683f7f12e072345fbf837727284524
Fabio Montezuma <fabio.montezuma@soaexpert.com.br>
2019-09-04 13:30:43 -0300

    Build VPC setup

--- service-role-build-policy.tf
+++ service-role-build-policy.tf
@@ -102,3 +102,36 @@ resource "aws_iam_role_policy" "codebuild-ecr" {
 }
 POLICY
 }
+
+resource "aws_iam_role_policy" "codebuild-vpc" {
+  name = "${var.project}-codebuild-vpc"
+  role = "${aws_iam_role.codebuild.name}"
+
+  policy = <<POLICY
+{
+    "Version": "2012-10-17",
+    "Statement": [
+        {
+            "Effect": "Allow",
+            "Action": [
+                "ec2:CreateNetworkInterface",
+                "ec2:DescribeDhcpOptions",
+                "ec2:DescribeNetworkInterfaces",
+                "ec2:DeleteNetworkInterface",
+                "ec2:DescribeSubnets",
+                "ec2:DescribeSecurityGroups",
+                "ec2:DescribeVpcs"
+            ],
+            "Resource": "*"
+        },
+        {
+            "Effect": "Allow",
+            "Action": [
+                "ec2:CreateNetworkInterfacePermission"
+            ],
+            "Resource": "*"
+        }
+    ]
+}
+POLICY
+}
